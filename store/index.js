4431d3a5ea6f957eaf4840df0b231f44e1a029f6
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2020-02-28 19:40:41 +0100

    make redux work

--- store/index.js
+++ store/index.js
@@ -1,9 +1,18 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import { AsyncStorage } from "react-native";
+import {createStore, compose, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+import logger from 'redux-logger';
 
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composeEnhancers =
   typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -11,14 +20,14 @@ const composeEnhancers =
 
 export function configureStore(initialState) {
   const store = createStore(
-    rootReducer,
+    persistedReducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger)),
   );
 
-  persistStore(store, { storage: AsyncStorage, blacklist: ['notifications'] }); //.purge();
-
-  return store;
+  console.log('store :', store);
+  let persistor = persistStore(store);
+  return {store, persistor};
 }
 
 export default configureStore();
